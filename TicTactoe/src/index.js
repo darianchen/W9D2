07ea3354c7601a25b96ba24ff2dfbf83ede772ed
Isaac <isaacc98@gmail.com>
2022-09-13 16:33:41 -0400

    DOMContent loaded eventListener

--- TicTactoe/src/index.js
+++ TicTactoe/src/index.js
@@ -1,62 +1,60 @@
-const View = require ()
-const Game = require ('game')
+
+const View = require('./ttt-view');
+const Game = require('../ttt_node/game');
 
 document.addEventListener("DOMContentLoaded", () => {
-  e.preventDefault();
+  // Your code here
+  let game = new Game();
+  let grid = document.querySelector('.ttt')
 
+  let view = new View(game, grid);
+});
 
+// const lsItems = JSON.parse(localStorage.getItem('items')) || [];
+// const lsRecipes = JSON.parse(localStorage.getItem('recipes')) || [];
 
-const lsItems = JSON.parse(localStorage.getItem('items')) || [];
-const lsRecipes = JSON.parse(localStorage.getItem('recipes')) || [];
+// //---------------PHASE 1 DOM MANIPULATION DEMO-----------------//
+// //create event handler that adds to our grocery list
+// const addItem = (e) => {
+//     e.preventDefault();
 
-//---------------PHASE 1 DOM MANIPULATION DEMO-----------------//
-//create event handler that adds to our grocery list
-const addItem = (e) => {
-    e.preventDefault();
+//     let input = document.querySelector("input[name='add-grocery']");
+//     let value = input.value;
 
-    let input = document.querySelector("input[name='add-grocery']");
-    let value = input.value;
+//     const item = { value: value } // { value: 'eggs' }
 
-    const item = { value: value } // { value: 'eggs' }
+//     // add this new item to our localStorage items
+//     lsItems.push(item);
+//     localStorage.setItem("items", JSON.stringify(lsItems));
 
-    // add this new item to our localStorage items
-    lsItems.push(item);
-    localStorage.setItem("items", JSON.stringify(lsItems));
+//     updateList();
 
-    updateList();
-
-    groceryForm.reset();
+//     groceryForm.reset();
     
-}
+// }
 
-//create action to render grocery list items
-const updateList = () => {
+// //create action to render grocery list items
+// const updateList = () => {
 
-    // method 1: using string html element
-    // groceries.innerHTML = lsItems.map(item => {
-    //     return `<li>${item.value}</li>`
-    // })
+//     // method 1: using string html element
+//     // groceries.innerHTML = lsItems.map(item => {
+//     //     return `<li>${item.value}</li>`
+//     // })
 
    
-    // method 2: using document.createElement
-    if (!groceries.hasChildNodes()) {
-        lsItems.map(item => {
-            let li = document.createElement('li');
-            li.innerText = item.value;
-            groceries.appendChild(li);
-        })
-    } else {
-        let li = document.createElement('li');
-        li.innerText = lsItems[lsItems.length - 1].value;
-        groceries.appendChild(li);
-    }
-}
+//     // method 2: using document.createElement
+//     if (!groceries.hasChildNodes()) {
+//         lsItems.map(item => {
+//             let li = document.createElement('li');
+//             li.innerText = item.value;
+//             groceries.appendChild(li);
+//         })
+//     } else {
+//         let li = document.createElement('li');
+//         li.innerText = lsItems[lsItems.length - 1].value;
+//         groceries.appendChild(li);
+//     }
+// }
 
-//create event handler to cross out list items on click
-const markAsDone = (e) => {
-    // e.stopPropagation();
+// //create event handler to cross out list items on click
 
-    // debugger
-    let ele = e.target; // list item of noodles
-    ele.classList.toggle('done');
-}
